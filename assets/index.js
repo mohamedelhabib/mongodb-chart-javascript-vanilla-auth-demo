db471527fc7dde218f7ba4c5fc98ba65ae84522f
Mohamed EL Habib <elhabib_med@yahoo.fr>
2022-03-01 06:58:54 +0100

    add more chart with interaction between them

--- assets/index.js
+++ assets/index.js
@@ -14,6 +14,7 @@ function getPass() {
 }
 
 function logOut() {
+  history.pushState(null,null,'#login');
   document.body.classList.toggle("logged-in", false);
 }
 
@@ -37,19 +38,73 @@ async function tryLogin() {
   client.auth.loginWithCredential(credential)
   // Realm.Credentials.emailPassword(getUser(), getPass())
   // app.logIn(credentials)
-  .then(() =>
-  {
+  .then(() => {
+    history.pushState(null,null,'#charts');
     const sdk = new ChartsEmbedSDK({
       baseUrl: BASE_URL, // Charts URL
       getUserToken: () => ChartsEmbedSDK.getRealmUserToken(client),
     });
+
+    const chart_zip = sdk.createChart({
+      chartId: CHART_ID_ZIP, // Chart ID
+    });
+
+    chart_zip.render(document.getElementById("chart_zip"))
+      .then(
+        () => chart_zip.addEventListener("click", 
+          (payload) => {
+            chart_cuisine.setFilter(payload.selectionFilter);
+            chart_score.setFilter(payload.selectionFilter);
+            chart.setFilter(payload.selectionFilter);
+          }
+        )
+      );
+
+    const chart_cuisine = sdk.createChart({
+      chartId: CHART_ID_CUISINE, // Chart ID
+    });
+
+    chart_cuisine.render(document.getElementById("chart_cuisine"))
+      .then(
+        () => chart_cuisine.addEventListener("click", 
+          (payload) => {
+            chart_zip.setFilter(payload.selectionFilter);
+            chart_score.setFilter(payload.selectionFilter);
+            chart.setFilter(payload.selectionFilter);
+          }
+        )
+      );
+    
+    const chart_score = sdk.createChart({
+      chartId: CHART_ID_SCORE, // Chart ID
+    });
+
+    chart_score.render(document.getElementById("chart_score"))
+      .then(
+        () => chart_score.addEventListener("click", 
+          (payload) => {
+            chart_zip.setFilter(payload.selectionFilter);
+            chart_cuisine.setFilter(payload.selectionFilter);
+            chart.setFilter(payload.selectionFilter);
+          }
+        )
+      );
 
     const chart = sdk.createChart({
       chartId: CHART_ID, // Chart ID
     });
 
     chart.render(document.getElementById("chart"));
+
     document.body.classList.toggle("logged-in", true);
+
+    document
+    .getElementById("refreshButton")
+    .addEventListener("click", () => {
+      chart_zip.refresh();
+      chart_cuisine.refresh();
+      chart.refresh();
+    });  
 
   }).catch(err => {
     console.error("Authentication failed. If you are using the pre-built sample, please use one of the listed email addresses and use 'password' as the password.")
